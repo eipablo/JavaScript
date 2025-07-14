e9b25cd6d66cdb97965194d942cafe48acba3c66
Pablo Vinícius <71195004+eipablo@users.noreply.github.com>
2025-07-14 18:44:59 -0300

    continuando

---
+++ udemy/aula9/exs/ex02.js
@@ -0,0 +1,9 @@
+let resultado1 = 2 + 3 * 5;
+let resultado2 = (2 + 3) * 5;
+let resultado3 = 4 + 3 ** 2;
+let resultado4 = 10 / 2 + 5 * 2;
+
+console.log(resultado1 === 17);
+console.log(resultado2 === 25);
+console.log(resultado3 === 13);
+console.log(resultado4 === 15);
