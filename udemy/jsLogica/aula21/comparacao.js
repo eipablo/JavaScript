49aa85b762b482c34966413262c2a5c6624a9b4b
Pablo Vinícius <71195004+eipablo@users.noreply.github.com>
2025-07-23 17:38:05 -0300

    continuando

---
+++ udemy/jsLogica/aula21/comparacao.js
@@ -0,0 +1,17 @@
+/* 
+? OPERADORES DE COMPARAÇÃO
+* > maior que
+* >= maior ou igual que
+* < menor que
+* <= menor ou igual que
+! == igualdade (valor) <- só checa valor, e não é recomendado utilizar *********************
+* === igualdade estrita (valor e tipo)
+! !=diferente que (valor) <- só checa valor, e não é recomendado utilizar *********************
+* !== diferente estrito (valor e tipo)
+*/
+
+const num1 = 2;
+const num2 = 4;
+const comp = num1 <= num2;
+
+console.log(comp);
---
+++ udemy/jsLogica/aula22/logico.js
@@ -0,0 +1,19 @@
+/*
+? OPERADORES LÓGICOS
+* && = AND -> E === Todas as expressões precisam ser verdadeiras para dar true
+* || = OR -> OU === Uma das expressões precisam ser verdadeiras para dar true
+* ! -> NOT -> NÃO === inverte uma expressão exemplo !false é igual a true
+*/
+
+/*
+? Avaliação de curto circuito 
+
+* False -> valor literal
+! Falsy {
+* 0
+* ' ' ` ` " " strings vazias
+* Null / Undefined
+* NaN 
+! } 
+? qualquer coisa que não está categorizado acima é avaliado como TRUE em JavaScript
+*/
