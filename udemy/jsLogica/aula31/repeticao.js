fe88e5aa3463b66635ee50c427f30a52934dcc01
Pablo Vinícius <71195004+eipablo@users.noreply.github.com>
2025-08-06 19:17:25 -0300

    estruturas de repetição

---
+++ udemy/jsLogica/aula31/repeticao.js
@@ -0,0 +1,35 @@
+//* utilizando o FOR
+
+
+// for(let i = 0; i <= 15; i++){
+//     const par = i % 2 === 0 ? 'é par' : 'é impar';
+//     console.log(i, par);
+// }
+
+const frutas = ['maçã', 'pera', 'uva', 'tangerina', 'manga']
+
+//* vai fazer o contador percorrer até o tamanho da array e vai mostrar a fruta representada por cada indice a partir do 0
+for (let i = 0; i < frutas.length; i++){
+    console.log(`Indice ${i} ${frutas[i]}`);
+}
+
+
+
+// for (let i = 0; i <= 100; i += 10){
+//     console.log(`Linha ${i}`);
+// }
+
+
+
+// for (let i = -10; i <= 10; i++){
+//     console.log(`Linha ${i}`);
+// }
+
+// for (let i = 10; i >= 0; i--){
+//     console.log(`Linha ${i}`);
+// }
+
+// for (let i = 100; i >= 0; i -= 10){
+//     console.log(`Linha ${i}`);
+// }
+
