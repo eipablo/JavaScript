8d7006c2b139c03a77b9ed3b7fe38bc5f0386c80
Pablo Vinícius <71195004+eipablo@users.noreply.github.com>
2025-08-07 19:12:51 -0300

    roman to int

---
+++ LeetCode/romanToInt/app.js
@@ -0,0 +1,40 @@
+//* Crie uma função JavaScript que receba uma string s representando um número romano e retorne o número inteiro equivalente.
+
+
+function romanToInt(s){
+    //* passo 1, vou mapear os valores dos simbolos romanos
+    const valores = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    };
+
+    let total = 0; //* criando uma variavel para guardar o total
+
+    for (let i = 0; i < s.length; i++){ //* looping para percorrer os index da string
+        const atual = valores[s[i]]; //* pega o valor atual
+        const proximo = valores[s[i + 1]] //* verifica o próximo valor
+
+        console.log(`Letra atual: ${s[i]} (${atual})`); //? debugando
+        console.log(`Próxima letra: ${s[i + 1] || 'nenhuma'} (${proximo || 0})`); //? debugando
+
+        if (proximo > atual){ //* se o próximo valor for maior que o atual ele subtrai ex: I(1) é menor que V(5) então ele subtrai o total (no caso aqui 10) pela letra atual que representa 1, sendo assim o resultado parcial será 9
+            total -= atual;
+            console.log(`Subtraindo: -${atual}`);
+        } else { //* em casos que a subtração não é necessária ele apenas soma
+            total += atual;
+            console.log(`Somando: +${atual}`);
+        }
+
+        console.log(`Total parcial: ${total}`);
+        console.log('----------------------');
+    };
+
+    return total;
+}
+
+console.log("Resultado final:", romanToInt("XIV"));
